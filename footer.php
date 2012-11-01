bc44de46491fee6b83ef19a3e2c05124c3b10a94
Preston <team@rideforthebrand.net>
2012-11-01 09:42:12 -0500

    Wordpress Skeleton
    Wordpress Skeleton

---
+++ footer.php
@@ -0,0 +1,20 @@
+	<!-- Footer -->
+	<footer>
+		
+		<!-- Copyright -->
+		<p class="copyright">
+			&copy; <?php echo date("Y"); ?> Copyright <?php bloginfo('name'); ?>. <?php _e('Powered by', 'html5blank'); ?> 
+			<a href="//wordpress.org" title="WordPress">WordPress</a> &amp; <a href="//html5blank.com" title="HTML5 Blank">HTML5 Blank</a>.
+		</p>
+		<!-- /Copyright -->
+		
+	</footer>
+	<!-- /Footer -->
+	
+	</div>
+	<!-- /Wrapper -->
+	
+	<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
+<!--[if IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
+<!--[if IE 8]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
+<!--[if gt IE 8]><!--><html <?php language_attributes(); ?> class="no-js"><!--<![endif]--> 
+<head>
+	<meta charset="UTF-8">
+	<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+	
+	<!-- Meta -->
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0;">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-status-bar-style" content="black">
+	
+	<meta name="description" content="<?php bloginfo('description'); ?>">
+	
+	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+		
+	<!-- CSS + jQuery + JavaScript -->
+	<?php wp_head(); ?>
+	
+</head>
+<body <?php body_class(); ?>>
+
+	<!-- Header -->
+	<header>
+	
+		<!-- Wrapper -->
+		<div class="wrapper">
+		
+			<!-- Logo -->
+			<div id="logo">
+				<a href="<?php echo home_url(); ?>">
+					<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
+				</a>
+			</div>
+			<!-- /Logo -->
+			
+			<!-- Nav -->
+			<nav>
+				<?php html5blank_nav(); ?>
+			</nav>
+			<!-- /Nav -->
+			
+			<br class="clear">
+			
+		</div>
+		<!-- /Wrapper -->
+	
+	</header>
+	<!-- /Header -->
+	
+	<!-- Wrapper -->
+	<div class="wrapper">
